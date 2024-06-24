bd46b92bad801fa97f11189c25f743d398b3fa42
GirafiStudios <faj10@msn.com>
2024-06-24 15:30:32 +0200

    Fixed compatibility with NeoForge 21.0.31-beta+. Closes #443

--- src/main/java/com/teammetallurgy/aquaculture/misc/NeptunesMight.java
+++ src/main/java/com/teammetallurgy/aquaculture/misc/NeptunesMight.java
@@ -8,19 +8,21 @@ import net.minecraft.world.item.ItemStack;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.common.NeoForgeMod;
-import net.neoforged.neoforge.event.entity.living.LivingHurtEvent;
+import net.neoforged.neoforge.common.damagesource.DamageContainer;
+import net.neoforged.neoforge.event.entity.living.LivingDamageEvent;
 
 @EventBusSubscriber(modid = Aquaculture.MOD_ID)
 public class NeptunesMight {
 
     @SubscribeEvent
-    public static void onAttack(LivingHurtEvent event) {
-        Entity source = event.getSource().getEntity();
+    public static void onAttack(LivingDamageEvent.Pre event) {
+        Entity source = event.getContainer().getSource().getEntity();
         if (source instanceof LivingEntity living) {
             if (living.isEyeInFluidType(NeoForgeMod.WATER_TYPE.value())) {
                 ItemStack heldStack = living.getMainHandItem();
                 if (heldStack.getItem() == AquaItems.NEPTUNIUM_SWORD.get() || heldStack.getItem() == AquaItems.NEPTUNIUM_AXE.get()) {
-                    event.setAmount(event.getAmount() * 1.5F);
+                    DamageContainer damage = event.getContainer();
+                    damage.setNewDamage(damage.getOriginalDamage() * 1.5F);
                 }
             }
         }
