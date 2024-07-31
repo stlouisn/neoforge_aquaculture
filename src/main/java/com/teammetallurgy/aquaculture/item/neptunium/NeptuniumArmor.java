5b42ec8561cfd54961419116ed0cbd9a0b9c4ebe
GirafiStudios <faj10@msn.com>
2024-07-31 15:32:08 +0200

    Cleanup

--- src/main/java/com/teammetallurgy/aquaculture/item/neptunium/NeptuniumArmor.java
+++ src/main/java/com/teammetallurgy/aquaculture/item/neptunium/NeptuniumArmor.java
@@ -16,7 +16,6 @@ import net.minecraft.world.item.ArmorMaterial;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.phys.Vec3;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.common.NeoForgeMod;
