85b924c64f5d98cb60994506cb23909e6e394147
stlouisn <stlouisn@hotmail.com>
2024-10-22 18:06:53 -0300

    updated deps and misc fixes

--- src/main/java/dev/aquaculture/api/AquacultureAPI.java
+++ src/main/java/dev/aquaculture/api/AquacultureAPI.java
@@ -4,16 +4,15 @@ import dev.aquaculture.Aquaculture;
 import dev.aquaculture.api.fish.FishData;
 import dev.aquaculture.api.fishing.Hook;
 import dev.aquaculture.init.AquaItems;
-import dev.aquaculture.init.FishRegistry;
 import dev.aquaculture.item.BaitItem;
 import dev.aquaculture.item.HookItem;
+import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.ItemTags;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.level.biome.Biome;
 import net.neoforged.neoforge.registries.DeferredItem;
-
-import javax.annotation.Nonnull;
 
 public class AquacultureAPI {
     /**
@@ -23,10 +22,6 @@ public class AquacultureAPI {
 
     public static BaitItem createBait(double useChance, int lureSpeedModifier) {
         return new BaitItem(useChance, lureSpeedModifier);
-    }
-
-    public static DeferredItem<Item> registerFishMount(@Nonnull String name) {
-        return FishRegistry.registerFishMount(name);
     }
 
     public static DeferredItem<Item> registerHook(Hook hook) {
@@ -40,8 +35,15 @@ public class AquacultureAPI {
         public static final TagKey<Item> BOBBER = tag(Aquaculture.MOD_ID, "bobber");
         public static final TagKey<Item> TACKLE_BOX = tag(Aquaculture.MOD_ID, "tackle_box");
 
+        @SuppressWarnings("unused")
+        public static final TagKey<Biome> EMPTY = biomeTag("aquaculture","empty");
+
         public static TagKey<Item> tag(String modID, String name) {
             return ItemTags.create(ResourceLocation.fromNamespaceAndPath(modID, name));
+        }
+
+        public static TagKey<Biome> biomeTag(String modID, String name) {
+            return TagKey.create(Registries.BIOME, ResourceLocation.fromNamespaceAndPath(modID, name));
         }
 
         public static void init() {
